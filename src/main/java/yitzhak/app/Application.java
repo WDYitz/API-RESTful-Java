db62d0f656a6c536d29cb900da70ebf62bc8d6fa
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-07-30 10:22:27 -0300

    feat: initial setup

---
+++ src/main/java/yitzhak/app/Application.java
@@ -0,0 +1,13 @@
+package yitzhak.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
